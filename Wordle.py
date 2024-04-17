a767f7c710a78eb7fef43fec0e74ce3465415701
Jaime Carlos Velez <74817361+vlzjc@users.noreply.github.com>
2024-04-17 14:17:43 +0800

    changes made

--- Wordle.py
+++ Wordle.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import random
-
+import time
+import os
 
 
 def checker():
@@ -10,9 +11,11 @@ def checker():
 	theWord = random.choice(wordlist) #chooses the random word
 	fin_Word = "['']".join(theWord) #clean the word
 	#print(fin_Word)
+	
 	atm = 6
 	while atm > 0:
-		attempt = str(input("Guess the word: "))
+		wordcheck = ""
+		attempt = str(input("Guess the word: ").upper())
 		if (len(attempt) != 5):
 			atm -= 1
 			print("only five letters allowed \n Remaining Attempts :", atm)
@@ -24,12 +27,27 @@ def checker():
 			print("Not quite right \n Remaining Attempts :", atm)
 			for char, word in zip(fin_Word,attempt):
 				if word in fin_Word and word in char:
-					print(word + " is in the right place")
+					wordcheck = wordcheck + " " + word + " "
 				elif word in fin_Word:
-					print(word + " is not in the right place ")
+					wordcheck = wordcheck + " + "
 				else:
-					print("✗ this is just wrong")
+					wordcheck = wordcheck + " x "
+			print (wordcheck)
 			if atm == 0:
 				print("Game Over and the word is :", fin_Word)
 		
+print("""	hi, this is my version of a game Wordle made with python.
+	The list of words selected in random are in the excel file.
+	Just like the game you have will 6 chances. 
+	In each attempt, you will be shown 
+	a + symbol if you have a correct letter but in the wrong position,
+	a x symbol if you are just wrong with the letter,
+	and the Letter for that position if that's correct.
+
+	May the odds be in your favor.
+	War. War never changes.
+	Toss a coin to your Witcher.
+	I don't know. Hakuna Matata. Good Luck!
+	""")
+
 checker()
